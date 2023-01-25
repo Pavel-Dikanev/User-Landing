4ced56704e528ac4d15f8d56a52953ab68201017
Pavel-Adlf <dikanev.pavel56@gmail.com>
2023-01-26 02:04:18 +0400

    feature: menu items and a lot of STYLING

--- src/modules/menu/index.tsx
+++ src/modules/menu/index.tsx
@@ -2,23 +2,35 @@ import React, { ReactElement } from 'react';
 import './styles.css' 
 import MenuImg from './../../images/menu.png'
 import TextImg from './../../images/text.png'
+import MaskImage from './../../images/mask.png'
 
 interface MessageProps {
     message: string;
 }
 
 
-function Menu(props: MessageProps):ReactElement {
+function Menu(props: MessageProps): ReactElement {
     return (
         <div className='module-contact-container'>
             <div className='grid-header'>USER OPTIONS</div>
 
             <div className='grid-nav'>
-                <img className='menuBackground' src={MenuImg} alt='menu'/>
+                <img className='menuBackground' src={MenuImg} alt='menu' />
+            </div>
+            <div className='grid-nav second'>
+                <div className='menuElement'>
+                    <img src={MaskImage} className='menuImg'/>
+                </div>
+                <div className='menuElement'>
+                    <img src={MaskImage} className='menuImg'/>
+                </div>
+                <div className='menuElement'>
+                    <img src={MaskImage} className='menuImg'/>
+                </div>
             </div>
 
             <div className='grid-desc'>
-                <img className='textBackground' src={TextImg} alt='menu'/>
+                <img className='textBackground' src={TextImg} alt='menu' />
             </div>
         </div>
     );
