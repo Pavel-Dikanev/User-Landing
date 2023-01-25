50ee2fd3ef442c9b65aeed03c8ed2123894ec504
Pavel-Adlf <dikanev.pavel56@gmail.com>
2023-01-25 04:30:54 +0400

    style: sticker

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import Ticker from './modules/ticker'
 import Menu from './modules/menu';
 import BackImage from './images/background.jpg'
+import StickerImage from './images/sticker.png'
 function App() {
   return (
     <div className="App">
       <img src={BackImage} className='image' alt="background"/>
+      <img src={StickerImage} className='sticker' alt="sticker"/>
       <Ticker/>
       <Menu message='Hello'/>
     </div>
